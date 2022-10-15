ce4e3e6a1796a425241e67e32b3c48f931e8b2e2
babu-x <bhaskar170863@gmail.com>
2022-10-15 12:12:59 +0000

    routing

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="about"
+      className="contact-img"
+    />
+    <h1 className="contact-des">Contact</h1>
+  </div>
+)
+
+export default Contact
